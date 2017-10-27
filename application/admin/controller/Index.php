ac3b78f26161c4bddb7e242cb867f2a7ee74b771
frambo <854181238@qq.com>
2017-10-27 12:24:48 +0800

    初始化框架

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Index extends Controller{
+
+	public function index()
+	{
+		echo "这是一个控制器";
+	}
+}
---
+++ application/admin/controller/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Test extends Controller{
+
+	public function test()
+	{
+		echo "自定义方法";
+	}
+}
